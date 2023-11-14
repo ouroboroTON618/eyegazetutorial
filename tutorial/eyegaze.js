f7305cd0a12f460bea5cdfb034caba6520ee60ef
Jashira Herrera Brito <herrerabrito.1@buckeyemail.osu.edu>
2023-11-14 13:22:24 -0500

    managed to import library

---
+++ tutorial/eyegaze.js
@@ -0,0 +1,17 @@
+import webgazer from 'webgazer'
+
+// main.js
+
+window.onload = function () {
+    webgazer.setGazeListener((data, elapsedTime) => {
+        if (data == null) {
+            return;
+        }
+
+        // Display gaze data on the webpage
+        document.getElementById('gazeData').innerText = `
+            Gaze X: ${data.x.toFixed(2)}
+            Gaze Y: ${data.y.toFixed(2)}
+        `;
+    }).begin();
+};
